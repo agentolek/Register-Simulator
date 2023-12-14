6c9572a9b590269aa5fc3590dde607c3da7362eb
agentolek <olek.nowak.20@gmail.com>
2023-12-14 16:24:44 +0100

    Started writing LogicGate class.

--- logic_gate.py
+++ logic_gate.py
@@ -1,3 +1,11 @@
 class LogicGate:
-    def __init__(self, entries, type) -> None:
+    def __init__(self, entries) -> None:
+        # TODO: add type checks for entries, avoid circular import somehow
+        self._entries = entries
+
+    def value(self):
         pass
+
+
+class AndGate(LogicGate):
+    pass
