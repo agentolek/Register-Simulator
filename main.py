34404f8285c7dc33b92d200011c795dcad31661a
agentolek <olek.nowak.20@gmail.com>
2023-12-27 20:26:49 +0100

    Project now writes sequences to file.

--- main.py
+++ main.py
@@ -30,10 +30,18 @@ def visualise_sequence(sequence, seq_number):
     """
     Prints the generated sequence in terminal.
     """
+
+    # Program only visualises the first 9999 sequences.
+    if seq_number > 9999:
+        return
+
     print_string = str(seq_number) + ". "
+    print_string = print_string.zfill(6)
     for elem in sequence:
         if elem:
-            print_string += "-"
+            # FIXME: uses an em dash which isn't in ASCII,
+            #        so might not work everywhere
+            print_string += "—"
         else:
             print_string += "_"
     print(print_string)
@@ -49,8 +57,8 @@ def run_for_steps(register: Register, steps: int):
 
     for _ in range(steps):
         current_values = register.values()
-        visualise_sequence(current_values, len(created_sequences))
         created_sequences.append(current_values)
+        visualise_sequence(current_values, len(created_sequences))
         register.update()
 
     return created_sequences
@@ -78,12 +86,24 @@ def run_until_loop(register: Register):
     return created_sequences
 
 
+def write_to_file(write_file_path, sequences):
+    try:
+        with open(write_file_path, "w") as f:
+            for sequence in sequences:
+                write_string = "".join(["1" if value else "0" for value in sequence])
+                f.write(f"{write_string}\n")
+    except OSError:
+        raise OSError("Something is wrong with the file path!")
+
+
 def main(arguments):
     args = parse_terminal_input(arguments)
     created_sequences = []
 
     register = parse_data(args.json_path)
-    if args.steps:
+
+    if args.steps is not None:
+        args.steps = int(args.steps)
         if args.steps <= 0:
             raise StepError
 
@@ -91,6 +111,8 @@ def main(arguments):
         created_sequences = run_until_loop(register)
     else:
         created_sequences = run_for_steps(register, args.steps)
+
+    write_to_file(args.destination_path, created_sequences)
 
 
 if __name__ == "__main__":
